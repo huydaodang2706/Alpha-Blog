4a60d513d3fc43b5fc7a912adea8b2dcd3e9a89d
huydaodang2706 <huydaodang2706@gmail.com>
2020-01-23 16:02:57 +0700

    Finish the front-end for index, show, edit, new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,11 +9,13 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    # set_article
   end
 
   def update
+    # set_article
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render :edit
@@ -24,7 +26,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render :new
@@ -32,11 +34,13 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    # set_article
   end
 
   def destroy
+    # set_article
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
   private
