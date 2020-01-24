bef9b448aaafa11c123624cfd0cb45d5a8a914be
huydaodang2706 <huydaodang2706@gmail.com>
2020-01-24 12:45:05 +0700

    Add user authentication pages and the user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_23_130738) do
+ActiveRecord::Schema.define(version: 2020_01_24_025127) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_01_23_130738) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
