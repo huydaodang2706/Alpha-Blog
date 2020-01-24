d75c6f3fbd73163b2147095e7c0ade735024d03d
huydaodang2706 <huydaodang2706@gmail.com>
2020-01-24 16:29:17 +0700

    Add show page and edit page for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -11,8 +13,23 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  def show
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "User account has successfully edited"
+      redirect_to articles_path
+    else
+      render :edit
+    end
   end
 
   private
