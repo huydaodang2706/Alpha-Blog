be775bf4b1255b57db11314ba0b35cfe5ec67d4e
huydaodang2706 <huydaodang2706@gmail.com>
2020-01-25 19:10:13 +0700

    Add login form and create and destroy session

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about',to: 'pages#about'
+
   get 'signup',to: 'users#new'
+
+  get 'login',to: 'sessions#new'
+  post 'login',to: 'sessions#create'
+  delete 'logout',to: 'sessions#destroy'
+
   resources :users, except: [:new]
   resources :articles
 end
